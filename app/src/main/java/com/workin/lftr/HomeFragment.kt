de3cc4aae85a5eff81e6b266dd384e85951c5af1
Aaron Springut <aspringut@gmail.com>
2023-02-05 17:05:04 +0000

    Initial commit

---
+++ app/src/main/java/com/workin/lftr/HomeFragment.kt
@@ -0,0 +1,56 @@
+package com.workin.lftr
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.workin.lftr.databinding.FragmentHomeBinding
+
+private const val TAG = "HomeFragment"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    private var binding: FragmentHomeBinding? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        val view = binding!!.root
+        binding?.createWorkoutButton?.setOnClickListener{this.createNewWorkout()}
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+    private fun createNewWorkout(){
+        Log.d(TAG, "Creating New Workout")
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment NewWorkoutFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() = HomeFragment()
+    }
+}
---
+++ app/src/main/java/com/workin/lftr/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.workin.lftr
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                val homeFragment = HomeFragment.newInstance()
+                add(R.id.mainFragmentContainerView, homeFragment)
+            }
+        }
+    }
+
+    fun transitionToCreateNewWorkout () {
+        supportFragmentManager.commit {
+
+        }
+    }
+
+}
